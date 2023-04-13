a53b6c6ab74dd39ec5eebc6e1dbb618f01cee0cd
Karrar Qasim <Karawr1996@gmail.com>
2023-04-12 21:26:33 -0600

    Create and connect componet and file structures

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import Articles from '../Articles/Articles';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      I am App
+      <Articles />
+    </main>
+  );
+}
+
+export default App;
