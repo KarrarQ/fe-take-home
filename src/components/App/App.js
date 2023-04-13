44428e76a5f9952a2a4b9a27b771e39b46d71ec5
Karrar Qasim <Karawr1996@gmail.com>
2023-04-13 10:20:26 -0600

    Add seach bar and functionality to display

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
 import Articles from '../Articles/Articles';
+import Search from '../Search/Search';
 import './App.css';
 
 function App() {
   const [articles, setArticles] = useState([]);
+  const [userSearch, setUserSearch] = useState('');
 
   useEffect(() => {
     fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=ybisF2CGGDEAJBCUhCzPdJMIdJoBGGDo')
@@ -19,6 +21,10 @@ function App() {
   return (
     <main>
       <h1>NY Times News Reader</h1>
+      <Search
+        userSearch={userSearch}
+        setUserSearch={setUserSearch} 
+      />
       <Articles
         articles={articles}
       />
